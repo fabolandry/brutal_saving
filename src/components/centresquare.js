3706aac4d7dbe0e52c3bd88dd63e0a5980b48b13
fabolandry <fabolandry@hotmail.com>
2022-04-10 09:59:56 -0700

    FE

---
+++ src/components/centresquare.js
@@ -0,0 +1,24 @@
+function CentreSquare() {
+    return (
+        <div className="mainsquare">
+            <p className="description">
+                You create a Smart Contract, 
+                That Smart Contract keeps your money 
+                from anyone in the world 
+                (You included) and pays you back 
+                at a set date. 
+            </p>
+            <form>
+                <label> Amount (in Eth) : </label>
+                <input type="text"  required/>
+            </form>
+            <form>
+                <label> Payback Date : </label>
+                <input className="date-pick" type="datetime-local"  required/>
+            </form>
+            <button className='submitbutton'>Send Transaction</button>   
+        </div>
+    )
+}
+
+export default CentreSquare
---
+++ src/components/connectwalletbutton.js
@@ -0,0 +1,9 @@
+function Connectwalletbutton() {
+    return (
+        <div>
+            <button className='connectwalletbutton'>Connect Wallet</button>
+        </div>
+    )
+}
+
+export default Connectwalletbutton
